af421a98b1613fe6b7fdec5b755137092bdb1165
Marco Rivera <marcogeofis@gmail.com>
2022-05-08 16:56:49 -0500

    se realizo un proyecto usando la libreria de tailwind para css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/index.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#f3c614',
+        'secundary':'#353535',
+      }
+    },
+  },
+  plugins: [],
+}
